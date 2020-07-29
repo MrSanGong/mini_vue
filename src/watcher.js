7519d04b509cadc2a5509a610c9dca0052937ec4
jiangzhanguang <857523776@qq.com>
2020-07-29 17:56:28 +0800

    fix: key赋错值

--- src/watcher.js
+++ src/watcher.js
@@ -15,14 +15,12 @@ class Watcher {
   constructor(vm, key, cb) {
     this.vm = vm
     // data中的属性名称
-    this.key = vm.key
+    this.key = key
     // 回调函数负责更新视图
     this.cb = cb
-    
-    // 1、把watcher对象记录到Dep类的静态属性target
+    // 把watcher对象记录到Dep类的静态属性target
     Dep.target = this
-    // 2、触发get方法，在get方法中会调用addSub
-    
+    // 触发get方法，在get方法中会调用addSub    
     this.oldValue = vm[key]
     Dep.target = null
   }
